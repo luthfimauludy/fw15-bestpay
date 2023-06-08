ad66f49f3b5019151b88a585cc668b9ced9bc2eb
luthfimauludy <putra.luthfi@gmail.com>
2023-06-08 13:57:57 +0700

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Nunito_Sans } from "next/font/google";
+
+const nunito = Nunito_Sans({
+  subsets: ["latin"],
+  variable: "--nunito-font",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --nunito-font: ${nunito.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />;
+    </>
+  );
 }
