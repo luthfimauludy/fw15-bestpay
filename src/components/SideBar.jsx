de015ef9747d4d093afab7863304f9021e3c3349
luthfimauludy <putra.luthfi@gmail.com>
2023-06-11 20:40:05 +0700

    feat: do logout

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,8 +4,16 @@ import { FiUser, FiArrowUp } from "react-icons/fi";
 import { RxDashboard } from "react-icons/rx";
 import { LuLogOut } from "react-icons/lu";
 import TopUp from "./TopUp";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function SideBar() {
+  const router = useRouter();
+  const doLogout = async () => {
+    await axios.get("http://localhost:3000/api/logout");
+    router.replace("/auth/login");
+  };
+
   return (
     <div className="flex flex-col justify-between min-w-[278px] max-h-full bg-white rounded-xl px-10 py-12">
       <div className="flex flex-col gap-[52px]">
@@ -23,10 +31,10 @@ export default function SideBar() {
           <p className="text-lg">Profile</p>
         </Link>
       </div>
-      <Link href="./auth/logout" className="flex items-center gap-6">
+      <button onClick={doLogout} className="flex items-center gap-6">
         <LuLogOut size={25} />
         <p className="text-lg">Logout</p>
-      </Link>
+      </button>
     </div>
   );
 }
