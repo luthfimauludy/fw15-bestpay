a532ef78836e78b3de45e76c604946bd488d7cab
luthfimauludy <putra.luthfi@gmail.com>
2023-06-13 16:23:07 +0700

    change: file name

--- src/pages/transfer/input-amount.jsx
+++ src/pages/transfer/input-amount.jsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import SideBar from "@/components/SideBar";
 
-export default function AmountInput() {
+export default function InputAmount() {
   return (
     <>
       <main className="bg-[#E5E5E5] h-full">
