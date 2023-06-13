46fd62bc29b5ec65f643ce18ef7078951a8415ca
luthfimauludy <putra.luthfi@gmail.com>
2023-06-14 02:41:23 +0700

    feat: transfer

--- src/pages/transfer/index.jsx
+++ src/pages/transfer/index.jsx
@@ -1,15 +1,147 @@
 import React from "react";
-import { FiSearch } from "react-icons/fi";
+import { FiSearch, FiUser } from "react-icons/fi";
+import Layout from "@/components/Layout";
+import cookieConfig from "@/helpers/cookieConfig";
+import checkCredentials from "@/helpers/checkCredentials";
+import { withIronSessionSsr } from "iron-session/next";
+import http from "@/helpers/http";
 import Image from "next/image";
-import defaultPic from "@/assets/default-picture.jpg";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import SideBar from "@/components/SideBar";
+import { useDispatch, useSelector } from "react-redux";
+import { setRecipient as setRecipientAction } from "@/redux/reducers/transfer";
+import { useRouter } from "next/router";
 
-export default function Transfer() {
+export const getServerSideProps = withIronSessionSsr(
+  async function getServerSideProps({ req, res }) {
+    const token = req.session?.token;
+    checkCredentials(token, res, "/auth/profile");
+    return {
+      props: {
+        userToken: token,
+      },
+    };
+  },
+  cookieConfig
+);
+
+export default function SearchRecipient({ userToken }) {
+  const dispatch = useDispatch();
+  const [search, setSearch] = React.useState("");
+  const [recipient, setRecipient] = React.useState({});
+  const router = useRouter();
+  const getUsers = React.useCallback(
+    async (page = 1, search = "") => {
+      const { data } = await http(userToken).get("/users", {
+        params: {
+          page,
+          search,
+        },
+      });
+      setRecipient(data);
+    },
+    [userToken]
+  );
+
+  React.useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  React.useEffect(() => {
+    getUsers(1, search);
+  }, [getUsers, search]);
+
+  const recipientRedux = useSelector((state) => state.transfer.user);
+
+  React.useEffect(() => {
+    if (recipientRedux) {
+      router.push("/transfer/input-amount");
+    }
+  }, [recipientRedux, router]);
+
   return (
     <>
-      <main className="bg-[#E5E5E5] h-full">
+      <Layout token={userToken}>
+        <div className="flex flex-col w-full h-full gap-5">
+          <div className="flex flex-col gap-5 min-w-[367px] bg-white p-[30px] rounded-xl">
+            <div className="flex justify-between items-center">
+              <p className="text-lg font-semibold">Search Receiver</p>
+            </div>
+            <div className="flex gap-4 items-center p-4 bg-gray-200 rounded-xl">
+              <FiSearch size={25} className="text-gray-400" />
+              <input
+                onChange={(e) => setSearch(e.target.value)}
+                className="w-full bg-transparent outline-none"
+                placeholder="Search receiver here"
+              />
+            </div>
+            {recipient.results && (
+              <>
+                {recipient.results.map((item) => (
+                  <div
+                    className="cursor-pointer flex justify-between items-center"
+                    key={`trx-list-${item.id}`}
+                  >
+                    <div
+                      onClick={() => dispatch(setRecipientAction(item))}
+                      className="flex justify-between items-center gap-3"
+                    >
+                      <>
+                        <div>
+                          {!item.picture && (
+                            <div className="w-12 h-12 bg-white border rounded flex justify-center items-center">
+                              <FiUser size={35} />
+                            </div>
+                          )}
+                          {item.picture && (
+                            <div className="w-12 h-12 border rounded-lg overflow-hidden">
+                              <Image
+                                className="object-fit rounded"
+                                width={100}
+                                height={100}
+                                src={item.picture}
+                                alt={item.fullName || item.email}
+                              />
+                            </div>
+                          )}
+                        </div>
+                        <div className="flex flex-col gap-2">
+                          <p className="font-semibold">{item.fullName}</p>
+                          <p className="text-sm text-[#6A6A6A]">{item.email}</p>
+                        </div>
+                      </>
+                    </div>
+                  </div>
+                ))}
+                <div className="flex justify-center items-center gap-4">
+                  <button
+                    onClick={() =>
+                      getUsers(recipient.pageInfo.page - 1, search)
+                    }
+                    disabled={recipient.pageInfo.page <= 1}
+                    className="btn bg-[#99A98F] border-none normal-case"
+                  >
+                    Prev
+                  </button>
+                  <div className="font-semibold">
+                    {recipient.pageInfo.page} of {recipient.pageInfo.totalPage}
+                  </div>
+                  <button
+                    onClick={() =>
+                      getUsers(recipient.pageInfo.page + 1, search)
+                    }
+                    disabled={
+                      recipient.pageInfo.page === recipient.pageInfo.totalPage
+                    }
+                    className="btn bg-[#99A98F] border-none normal-case"
+                  >
+                    Next
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </Layout>
+      {/* <main className="bg-[#E5E5E5] h-full">
         <Navbar />
         <div className="flex w-full px-[150px] py-10 gap-5">
           <SideBar />
@@ -36,44 +168,11 @@ export default function Transfer() {
                   </div>
                 </div>
               </div>
-              <div className="flex justify-between items-center">
-                <div className="flex items-center w-full min-h-[110px] shadow-lg shadow-gray-300/50 rounded-xl p-5 gap-5">
-                  <div className="w-14 h-14">
-                    <Image src={defaultPic} alt="Default picture" />
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <p className="font-semibold">Momo Taro</p>
-                    <p className="text-sm text-[#6A6A6A]">+62 812-4343-6731</p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex justify-between items-center">
-                <div className="flex items-center w-full min-h-[110px] shadow-lg shadow-gray-300/50 rounded-xl p-5 gap-5">
-                  <div className="w-14 h-14">
-                    <Image src={defaultPic} alt="Default picture" />
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <p className="font-semibold">Jessica Keen</p>
-                    <p className="text-sm text-[#6A6A6A]">+62 811-3452-5252</p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex justify-between items-center">
-                <div className="flex items-center w-full min-h-[110px] shadow-lg shadow-gray-300/50 rounded-xl p-5 gap-5">
-                  <div className="w-14 h-14">
-                    <Image src={defaultPic} alt="Default picture" />
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <p className="font-semibold">Michael Le</p>
-                    <p className="text-sm text-[#6A6A6A]">+62 810-4224-4613</p>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
         <Footer />
-      </main>
+      </main> */}
     </>
   );
 }
