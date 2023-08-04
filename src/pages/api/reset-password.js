681b960a731a86127e9e0fe5b86d98bf9d59454e
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-08-04 22:06:30 +0700

    feat: request api reset password

---
+++ src/pages/api/reset-password.js
@@ -0,0 +1,18 @@
+import coockieConfig from "@/helpers/cookieConfig";
+import { withIronSessionApiRoute } from "iron-session/next";
+
+export default withIronSessionApiRoute(async function resetRoute(req, res) {
+  const request = await fetch(
+    process.env.NEXT_PUBLIC_BACKEND_URL + "/auth/reset-password",
+    {
+      method: "POST",
+      body: new URLSearchParams(req.body).toString(),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+
+  const respons = await request.json();
+  return res.json(respons);
+}, coockieConfig);
