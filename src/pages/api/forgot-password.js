2867eac72e94aa9c4dfa7959639e80f3d249f6c7
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-08-04 22:05:41 +0700

    feat: request api forgot password

---
+++ src/pages/api/forgot-password.js
@@ -0,0 +1,18 @@
+import cookieConfig from "@/helpers/cookieConfig";
+import { withIronSessionApiRoute } from "iron-session/next";
+
+export default withIronSessionApiRoute(async function forgotRoute(req, res) {
+  const request = await fetch(
+    process.env.NEXT_PUBLIC_BACKEND_URL + "/auth/forgot-password",
+    {
+      method: "POST",
+      body: new URLSearchParams(req.body).toString(),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+
+  const respons = await request.json();
+  return res.json(respons);
+}, cookieConfig);
