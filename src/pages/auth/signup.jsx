67b7cc93ac00ade4087b2a46ffe314a02d9cd9d3
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-08-07 00:39:03 +0700

    fix: post path name

--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -8,10 +8,11 @@ import { Formik } from "formik";
 import { useRouter } from "next/router";
 import { withIronSessionSsr } from "iron-session/next";
 import cookieConfig from "@/helpers/cookieConfig";
-import http from "@/helpers/http";
 import { saveEmail } from "@/redux/reducers/auth";
 import { useDispatch } from "react-redux";
 import { FaBehanceSquare } from "react-icons/fa";
+import axios from "axios";
+import Head from "next/head";
 
 export const getServerSideProps = withIronSessionSsr(
   async function getServerSideProps({ req, res }) {
@@ -37,21 +38,22 @@ export const getServerSideProps = withIronSessionSsr(
   cookieConfig
 );
 
+const validationSchema = Yup.object({
+  username: Yup.string()
+    .min(3, "Username must have at least 3 characters")
+    .required("Username cannot be empty"),
+  email: Yup.string()
+    .email("Email is not valid")
+    .required("Email cannot be empty"),
+  password: Yup.string().required("Password cannot be empty"),
+});
+
 export default function Signup() {
   const dispatch = useDispatch();
   const router = useRouter();
   const [errorMessage, setErrorMessage] = React.useState("");
   const [isLoading, setLoading] = React.useState("");
   const [eye, setEye] = React.useState(false);
-  const validationSchema = Yup.object({
-    username: Yup.string()
-      .min(3, "Username must have at least 3 characters")
-      .required("Username cannot be empty"),
-    email: Yup.string()
-      .email("Email is not valid")
-      .required("Email cannot be empty"),
-    password: Yup.string().required("Password cannot be empty"),
-  });
 
   function showEye() {
     setEye(!eye);
@@ -67,9 +69,11 @@ export default function Signup() {
         password: values.password,
       }).toString();
 
-      const { data } = await http().post("/auth/register", form);
-      dispatch(saveEmail(values.email));
-      router.push("/auth/set-pin");
+      const { data } = await axios.post("/api/signup", form);
+      if (data.success === true) {
+        dispatch(saveEmail(values.email));
+        router.push("/auth/set-pin");
+      }
     } catch (err) {
       console.log(err);
       const message = err?.response?.data?.message;
@@ -82,171 +86,175 @@ export default function Signup() {
   };
 
   return (
-    <main className="bg-[#E5E5E5]">
-      <div className="flex md:flex-row flex-col min-h-screen">
-        {/* Window */}
-        <div className="hidden md:flex flex-col flex-1 bg-header bg-cover bg-no-repeat py-[50px] px-[100px] text-white">
-          <div className="flex items-center font-semibold">
-            <FaBehanceSquare size={45} color="black" />
-            <p className="font-bold text-[29px]">stPay</p>
-          </div>
-          <div>
-            <Image src={phone} alt="Phone" />
-          </div>
-          <div className="text-2xl font-bold pb-8">
-            App that Covering Banking Needs.
-          </div>
-          <div className="max-w-[497px]">
-            BestPay is an application that focussing in banking needs for all
-            users in the world. Always updated and always following world
-            trends. 5000+ users registered in BestPay everyday with worldwide
-            users coverage.
-          </div>
-        </div>
-
-        {/* Mobile */}
-        <div className="md:hidden text-center px-10 pt-[100px] pb-[60px] text-[29px] font-bold">
-          BestPay
-        </div>
-        <div className="w-full md:max-w-[550px] bg-white md:bg-[#E5E5E5] rounded-t-3xl md:rounded-none py-10 md:py-[80px] px-4 md:pl-12 md:pr-[80px]">
-          {/* Mobile */}
-          <div className="md:hidden flex flex-col justify-center items-center gap-8">
-            <p className="max-w-[394px] text-2xl font-bold leading-loose">
-              Sign Up
-            </p>
-            <p className="max-w-[290px] text-center text-gray-400 leading-loose">
-              Create your account to access BestPay.
-            </p>
-          </div>
-
+    <>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <main className="bg-[#E5E5E5]">
+        <div className="flex md:flex-row flex-col min-h-screen">
           {/* Window */}
-          <div className="hidden md:flex flex-col gap-8">
-            <p className="max-w-[394px] text-2xl font-bold leading-loose">
-              Start Accessing Banking Needs With All Devices and All Platforms
-              With 30.000+ Users
-            </p>
-            <p className="leading-loose">
-              Transfering money is eassier than ever, you can access BestPay
-              wherever you are. Desktop, laptop, mobile phone? we cover all of
-              that for you!
-            </p>
+          <div className="hidden md:flex flex-col flex-1 bg-header bg-cover bg-no-repeat py-[50px] px-[100px] text-white">
+            <div className="flex items-center font-semibold">
+              <FaBehanceSquare size={45} color="black" />
+              <p className="font-bold text-[29px]">stPay</p>
+            </div>
+            <div>
+              <Image src={phone} alt="Phone" />
+            </div>
+            <div className="text-2xl font-bold pb-8">
+              App that Covering Banking Needs.
+            </div>
+            <div className="max-w-[497px]">
+              BestPay is an application that focussing in banking needs for all
+              users in the world. Always updated and always following world
+              trends. 5000+ users registered in BestPay everyday with worldwide
+              users coverage.
+            </div>
           </div>
-          <Formik
-            onSubmit={doSignup}
-            initialValues={{
-              username: "",
-              email: "",
-              password: "",
-            }}
-            validationSchema={validationSchema}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleBlur,
-              handleSubmit,
-              handleChange,
-              isSubmitting,
-            }) => {
-              return (
-                <form onSubmit={handleSubmit} className="pt-16">
-                  {errorMessage && (
-                    <div className="flex justify-center alert bg-red-500 border-none shadow-lg mb-5 text-white">
-                      {errorMessage}
+
+          {/* Mobile */}
+          <div className="md:hidden text-center px-10 pt-[100px] pb-[60px] text-[29px] font-bold">
+            BestPay
+          </div>
+          <div className="w-full md:max-w-[550px] bg-white md:bg-[#E5E5E5] rounded-t-3xl md:rounded-none py-10 md:py-[80px] px-4 md:pl-12 md:pr-[80px]">
+            {/* Mobile */}
+            <div className="md:hidden flex flex-col justify-center items-center gap-8">
+              <p className="max-w-[394px] text-2xl font-bold leading-loose">
+                Sign Up
+              </p>
+              <p className="max-w-[290px] text-center text-gray-400 leading-loose">
+                Create your account to access BestPay.
+              </p>
+            </div>
+
+            {/* Window */}
+            <div className="hidden md:flex flex-col gap-8">
+              <p className="max-w-[394px] text-2xl font-bold leading-loose">
+                Start Accessing Banking Needs With All Devices and All Platforms
+                With 30.000+ Users
+              </p>
+              <p className="leading-loose">
+                Transfering money is eassier than ever, you can access BestPay
+                wherever you are. Desktop, laptop, mobile phone? we cover all of
+                that for you!
+              </p>
+            </div>
+            <Formik
+              onSubmit={doSignup}
+              initialValues={{
+                username: "",
+                email: "",
+                password: "",
+              }}
+              validationSchema={validationSchema}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleSubmit,
+                handleChange,
+              }) => {
+                return (
+                  <form onSubmit={handleSubmit} className="pt-16">
+                    {errorMessage && (
+                      <div className="flex justify-center alert bg-red-500 border-none shadow-lg mb-5 text-white">
+                        {errorMessage}
+                      </div>
+                    )}
+                    <div className="flex border-b border-gray-500 gap-5 pt-10 pb-4">
+                      <FiUser size={35} />
+                      <input
+                        type="text"
+                        name="username"
+                        className="w-full bg-transparent rounded-lg border-none outline-none"
+                        placeholder="Enter your username"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.username}
+                      />
                     </div>
-                  )}
-                  <div className="flex border-b border-gray-500 gap-5 pt-10 pb-4">
-                    <FiUser size={35} />
-                    <input
-                      type="text"
-                      name="username"
-                      className="w-full bg-transparent rounded-lg border-none outline-none"
-                      placeholder="Enter your username"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.username}
-                    />
-                  </div>
-                  {errors.username && touched.username && (
-                    <label className="label">
-                      <span className="label-text-alt text-red-500">
-                        {errors.username}
-                      </span>
-                    </label>
-                  )}
-                  <div className="flex border-b border-gray-500 gap-5 pt-10 pb-4">
-                    <FiMail size={35} />
-                    <input
-                      type="email"
-                      name="email"
-                      className="w-full bg-transparent rounded-lg border-none outline-none"
-                      placeholder="Enter your e-mail"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.email}
-                    />
-                  </div>
-                  {errors.email && touched.email && (
-                    <label className="label">
-                      <span className="label-text-alt text-red-500">
-                        {errors.email}
-                      </span>
-                    </label>
-                  )}
-                  <div className="flex border-b border-gray-500 gap-5 pt-10 pb-4 relative">
-                    <FiLock size={35} />
-                    <input
-                      type={eye ? "text" : "password"}
-                      name="password"
-                      className="w-full bg-transparent rounded-lg border-none outline-none"
-                      placeholder="Enter your password"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.password}
-                    />
-                    <button type="button" onClick={showEye}>
-                      {eye ? (
-                        <FiEye
-                          className="flex justify-center items-center right-2 bottom-4 absolute"
-                          size={25}
-                        />
-                      ) : (
-                        <FiEyeOff
-                          className="flex justify-center items-center right-2 bottom-4 absolute"
-                          size={25}
-                        />
-                      )}
-                    </button>
-                  </div>
-                  {errors.password && touched.password && (
-                    <label className="label">
-                      <span className="label-text-alt text-red-500">
-                        {errors.password}
-                      </span>
-                    </label>
-                  )}
-                  <div className="pb-10 pt-20">
-                    <button
-                      disabled={isLoading}
-                      className="btn bg-gray-300 hover:bg-[#99A98F] border-none btn-block normal-case"
-                      type="submit"
-                    >
-                      Sign Up
-                    </button>
-                  </div>
-                  <div className="flex justify-center">
-                    Don’t have an account? Let’s&nbsp;
-                    <Link href="./login" className="text-[#99A98F] font-bold">
-                      Login
-                    </Link>
-                  </div>
-                </form>
-              );
-            }}
-          </Formik>
+                    {errors.username && touched.username && (
+                      <label className="label">
+                        <span className="label-text-alt text-red-500">
+                          {errors.username}
+                        </span>
+                      </label>
+                    )}
+                    <div className="flex border-b border-gray-500 gap-5 pt-10 pb-4">
+                      <FiMail size={35} />
+                      <input
+                        type="email"
+                        name="email"
+                        className="w-full bg-transparent rounded-lg border-none outline-none"
+                        placeholder="Enter your e-mail"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                      />
+                    </div>
+                    {errors.email && touched.email && (
+                      <label className="label">
+                        <span className="label-text-alt text-red-500">
+                          {errors.email}
+                        </span>
+                      </label>
+                    )}
+                    <div className="flex border-b border-gray-500 gap-5 pt-10 pb-4 relative">
+                      <FiLock size={35} />
+                      <input
+                        type={eye ? "text" : "password"}
+                        name="password"
+                        className="w-full bg-transparent rounded-lg border-none outline-none"
+                        placeholder="Enter your password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                      />
+                      <button type="button" onClick={showEye}>
+                        {eye ? (
+                          <FiEye
+                            className="flex justify-center items-center right-2 bottom-4 absolute"
+                            size={25}
+                          />
+                        ) : (
+                          <FiEyeOff
+                            className="flex justify-center items-center right-2 bottom-4 absolute"
+                            size={25}
+                          />
+                        )}
+                      </button>
+                    </div>
+                    {errors.password && touched.password && (
+                      <label className="label">
+                        <span className="label-text-alt text-red-500">
+                          {errors.password}
+                        </span>
+                      </label>
+                    )}
+                    <div className="pb-10 pt-20">
+                      <button
+                        disabled={isLoading}
+                        className="btn bg-gray-300 hover:bg-[#99A98F] border-none btn-block normal-case"
+                        type="submit"
+                      >
+                        Sign Up
+                      </button>
+                    </div>
+                    <div className="flex justify-center">
+                      Already have an account? Let’s&nbsp;
+                      <Link href="./login" className="text-[#99A98F] font-bold">
+                        Login
+                      </Link>
+                    </div>
+                  </form>
+                );
+              }}
+            </Formik>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
