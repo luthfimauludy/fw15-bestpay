ddad210d1ced0ecfa08007c969fade5bac1065cc
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-08-07 00:29:56 +0700

    fix: styling sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,24 +4,52 @@ import { FiUser, FiArrowUp } from "react-icons/fi";
 import { RxDashboard } from "react-icons/rx";
 import TopUp from "./TopUp";
 import Logout from "./Logout";
+import { useRouter } from "next/router";
+
+const MENU = [
+  {
+    id: 1,
+    name: "Dashboard",
+    href: "/home",
+    icon: <RxDashboard size={25} />,
+  },
+  {
+    id: 2,
+    name: "Transfer",
+    href: "/transfer",
+    icon: <FiArrowUp size={25} />,
+  },
+  {
+    id: 3,
+    name: "Profile",
+    href: "/profile",
+    icon: <FiUser size={25} />,
+  },
+];
 
 export default function SideBar() {
+  const router = useRouter();
+
   return (
-    <div className="flex flex-col justify-between min-w-[278px] max-h-full bg-white rounded-xl px-10 py-12">
+    <div className="flex flex-col justify-between min-w-[278px] max-h-full bg-white rounded-xl py-12">
       <div className="flex flex-col gap-[52px]">
-        <Link href="/home" className="flex items-center gap-6">
-          <RxDashboard size={25} />
-          <p className="text-lg">Dashboard</p>
-        </Link>
-        <Link href="/transfer" className="flex items-center gap-6">
-          <FiArrowUp size={25} />
-          <p className="text-lg">Transfer</p>
-        </Link>
+        {MENU.map((item) => (
+          <div key={`menu-${item.id}`}>
+            <Link
+              href={item.href}
+              className={
+                "flex items-center gap-6 border-l-4 px-10" +
+                (item.href === router.pathname
+                  ? " border-[#6A6A6A]"
+                  : " border-transparent")
+              }
+            >
+              <span>{item.icon}</span>
+              <span className="text-lg">{item.name}</span>
+            </Link>
+          </div>
+        ))}
         <TopUp />
-        <Link href="/profile" className="flex items-center gap-6">
-          <FiUser size={25} />
-          <p className="text-lg">Profile</p>
-        </Link>
       </div>
       <Logout />
     </div>
